4d164d94c438597ff31a26adddc7d140260dc76e
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2022-12-13 08:51:20 -0300

    chore: change providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,22 @@
 
 namespace App\Providers;
 
+use App\Infrastructure\Repository\Booking\BlockRepository;
+use App\Infrastructure\Repository\Booking\BookingRepository;
+use App\Infrastructure\Repository\Booking\IBlockRepository;
+use App\Infrastructure\Repository\Booking\IBookingRepository;
+use App\Infrastructure\Repository\Room\IRoomRepository;
+use App\Infrastructure\Repository\Room\RoomRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public $bindings = [
+        IBookingRepository::class => BookingRepository::class,
+        IRoomRepository::class => RoomRepository::class,
+        IBlockRepository::class => BlockRepository::class
+    ];
+
     /**
      * Register any application services.
      *
