be194326f5ec18ea8be18ae65fda6d5d72c2b619
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2022-12-13 08:49:15 -0300

    chore: add routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Booking\BookingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('booking')->group(function () {
+    Route::get('daily-occupancy-rates/{date}', [BookingController::class, 'calculateDailyOccupancyRate']);
+    Route::get('monthly-occupancy-rates/{date}', [BookingController::class, 'calculateMonthlyOccupancyRate']);
+
+    Route::post('createBooking', [BookingController::class, 'createBooking']);
+
+    Route::put('updateBooking', [BookingController::class, 'updateBooking']);
 });
